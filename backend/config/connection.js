918765d99629cefe42b97dde0b315c9edbd48541
AlphaBoothes05 <73681471+AlphaBoothes05@users.noreply.github.com>
2021-06-07 18:46:08 -0600

    correcciones backend

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -4,7 +4,7 @@ const objectConnection = {
     "host": "localhost",
     "port": "3306",
     "user": "root",
-    "password": "gym05062015",
+    "password": "mYR00tpasswurd",
     "database": "proyecto"
 }
 
